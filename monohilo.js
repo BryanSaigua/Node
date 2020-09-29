5fcdbe8fbb18f548c2f73d49e1f3d3111f286479
Bryan Saigua <bryan-alexander2013@hotmail.com>
2020-09-29 18:25:52 +0200

    inicializacion del proyecto node

---
+++ monohilo.js
@@ -0,0 +1,14 @@
+console.log('hola mundo');
+
+let i = 0;
+
+setInterval(function(){ //intervalos de tiempo
+  console.log(i);
+  i++;
+
+  /*if(i === 5){// error
+    var a = 3 + z;
+  }*/
+}, 1000);
+
+console.log('segunda instruccion');
