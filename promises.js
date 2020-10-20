f02a13e024ff1923b973af4125c75e1e5d78adab
Bryan Saigua <bryan-alexander2013@hotmail.com>
2020-10-20 17:17:14 +0200

    promise, siempre que pongmaos un then hay que poner un catch para que no pase a errores mayores

--- promises.js
+++ promises.js
@@ -1,21 +1,41 @@
-function hola(nombre, micallback) {
-  console.log('hola soy una funcion asincrona');
-  setTimeout(function () { //funcion de tiempo
-    console.log('Hola, '+ nombre);
-    micallback(nombre);
-  }, 1000);
+function hola(nombre) {
+  return  new Promise((resolve, reject) => {
+      setTimeout(function () { //funcion de tiempo
+        console.log('Hola, '+ nombre);
+          resolve(nombre);
+        }, 1000);
+  });
 }
 
-function hablar(callbackHablar){
-  setTimeout(function(){
-    console.log('Bla bla bla bla ....');
-    callbackHablar();
-  },1000);
+function hablar(nombre){
+  return new Promise( (resolve,reject) => {
+    setTimeout(function(){
+      console.log('Bla bla bla bla ....');
+      //resolve(nombre);
+      reject('Hay un error');
+    },1000);
+    });
 }
 
-function adios(nombre, otroCallBack){
-  setTimeout(function () { //funcion de tiempo
-    console.log('Adios, '+ nombre);
-    otroCallBack();
-  }, 1000);
-};
+function adios(nombre){
+  return new Promise((resolve, reject) =>{
+    setTimeout(function () { //funcion de tiempo
+      console.log('Adios, '+ nombre);
+      resolve(nombre);
+    }, 1000);
+  });
+}
+
+// ---
+console.log('Iniciando el proceso...');
+hola('Bryan')
+    .then(hablar)
+    .then(adios)
+    .then(( nombre ) => {
+        console.log('Terminado el proceso...');
+    })
+
+    .catch(error =>{
+      console.error('Ha habido un error:');
+      console.error(error);
+    })
