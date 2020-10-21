57c9aa384388389f5294b3f3ae8e3fdb91bd0e47
Bryan Saigua <bryan-alexander2013@hotmail.com>
2020-10-22 01:15:49 +0200

    funciones variables globales

---
+++ modulos/globales.js
@@ -0,0 +1,12 @@
+/*let i = 0;
+let intervalo = setInterval(function () {
+  console.log('hola');
+  if (i === 3) {
+    clearInterval(intervalo)
+  }
+  i++;
+},1000);*/
+
+/*setImmediate(function () { //inmediatos 
+  console.log('Hola');
+});*/
